aa3fc1aec372701100bea3b14390d8bcecd0ee0c
shree-1617 <shreeti.parihar02@gmail.com>
2025-12-29 16:12:42 +0530

    Client and server created successfully

--- Server.java
+++ Server.java
@@ -1,50 +1,38 @@
-
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.function.Consumer;
 
-public class Server{
-       public Consumer<Socket> getConsumer(){
-        // return new Consumer<Socket>(){
-        //     @Override
-        //     public void accept(Socket clientSocket){
-        //        try {
-        //         PrintWriter toClient = new PrintWriter(clientSocket.getOutputStream());
-        //         toClient.println("Hello, from server!");
-        //         toClient.close();
-        //         clientSocket.close();
-        //         } catch (Exception e) {
-        //         }
-        //     }
-        //  };
-             return (clientSocket)->{
-                try {
-                PrintWriter toClient = new PrintWriter(clientSocket.getOutputStream());
-                toClient.println("Hello, from server!");
-                toClient.close();
-                clientSocket.close();
-                } catch (Exception e) {
-                }
-             };
-       }
-
-
-        public static void main(String[] args) {
-        int port = 8080; // Default port
-        Server server = new Server();
-        try {
-           ServerSocket serverSocket = new ServerSocket(port);
-           serverSocket.setSoTimeout(100000);
-           System.out.println("Server started on port " + port);
-           while (true) {
-              Socket acceptedSocket = serverSocket.accept();
-              Thread thread = new Thread(()->server.getConsumer().accept(acceptedSocket));
-              thread.start();
-           }    
-
-        } catch (Exception e) {
-           e.printStackTrace();    
-         }
+public class Server {
+    public Consumer<Socket> getConsumer() {
+        return (clientSocket) -> {
+            try (PrintWriter toSocket = new PrintWriter(clientSocket.getOutputStream(), true)) {
+                toSocket.println("Hello from server " + clientSocket.getInetAddress());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        };
     }
+    
+    public static void main(String[] args) {
+        int port = 8010;
+        Server server = new Server();
+        
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            serverSocket.setSoTimeout(70000);
+            System.out.println("Server is listening on port " + port);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                
+                // Create and start a new thread for each client
+                Thread thread = new Thread(() -> server.getConsumer().accept(clientSocket));
+                thread.start();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
 }
